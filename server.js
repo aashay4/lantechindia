835b21fdf43f8fae1d4df71f1f5dc9ddb3add274
Aashay <aashay.trivedi1@gmail.com>
2020-03-22 16:31:04 +0530

    first

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.post('/api/note/create', (req, res) => {
     service: 'Gmail',
     auth: {
         user: 'aashay.trivedi1@gmail.com',
-        pass: 'blur_shark9898'
+        pass: 'aashaytrivedi2'
     },
     tls:{
         rejectUnauthorized: false
