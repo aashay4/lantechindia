4d495b7fbdb1f5d68044f98f13e1e8ae3e5ce552
Aashay <aashay.trivedi1@gmail.com>
2021-05-26 10:10:20 +0530

    first

--- src/views/layer3switch.vue
+++ src/views/layer3switch.vue
@@ -177,7 +177,8 @@ export default {
        meta: [
 
          {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-         {name: 'description', content: 'Learn about configuring layer 3 switch.'}
+         {name: 'description', content: 'Learn about configuring layer 3 switch.'},
+         {property: 'og:image', content: 'https://firebasestorage.googleapis.com/v0/b/dev-blog-2503f.appspot.com/o/postsStorage%2F-KxXdvvLqDHBcxdUfLgn%2Fonfleck?alt=media&token=24a9bf5b-dce2-46e8-b175-fb63f7501c98'}
        ],
        link: [
       {rel: 'canonical', href: 'https://lantechinfocom.co.in/configure-layer-3-switch/'}
